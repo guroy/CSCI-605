2ed0bc146199fdc9f82e6148daea654699e9a44d
guroy <guroy@insa-rennes.fr>
2015-09-16 13:37:46 -0400

    Create class Connect4FieldInterface

---
+++ CSCI_605_Homework_04/src/Connect4Field.java
@@ -0,0 +1,40 @@
+
+public class Connect4Field implements Connect4FieldInterface {
+
+	@Override
+	public boolean checkIfPiecedCanBeDroppedIn(int column) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void dropPieces(int column, char gamePiece) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean didLastMoveWin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isItaDraw() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init(PlayerInterface playerA, PlayerInterface playerB) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void playTheGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
