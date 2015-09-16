fb0b1a4d4a2b295f3adbe0665971fb549ec9027c
Gurvan <g.lecuyer22@gmail.com>
2015-09-16 13:20:00 -0400

    Ajout du Homework_04 avec des fichiers indésirable

---
+++ CSCI_605_Homework_04/src/PlayerInterface.java
@@ -0,0 +1,9 @@
+public interface PlayerInterface {
+
+// this is how your constructor has to be
+// Player(Connect4FieldInterface theField, String name, char gamePiece)
+	
+	public char getGamePiece();
+	public String getName();
+	public int  nextMove();
+}
