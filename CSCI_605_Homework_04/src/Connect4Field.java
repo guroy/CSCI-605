d60990c784e5309d85ee76a61ec621c16590cfd8
guroy <guroy@insa-rennes.fr>
2015-09-21 16:07:06 -0400

    Update Connect4Field.java

--- CSCI_605_Homework_04/src/Connect4Field.java
+++ CSCI_605_Homework_04/src/Connect4Field.java
@@ -2,46 +2,46 @@
 public class Connect4Field implements Connect4FieldInterface {
 	static final int BOARD_WIDTH = 25;
 	static final int BOARD_HEIGHT = 9;
-	
+
 	char[][] board;
-	PlayerInterface player1;
-	PlayerInterface player2;
-	boolean isPlayer1Turn;
-	boolean endgame;
+	PlayerInterface thePlayers[];
 	Position lastMovePosition;
-	
+
 	// store the position of a piece in the board
 	class Position { 
 		int X;
 		int Y;
 	}
-	
+
 	public Connect4Field() {
 		super();
-		endgame = false;
-		
-		board = new char[BOARD_WIDTH][BOARD_HEIGHT];
+
+		board = new char[BOARD_HEIGHT][BOARD_WIDTH];
 		int thin = 0;
-		
+
 		// create game board
 		for (int j = 0; j < BOARD_HEIGHT; j++)
 		{
 			for (int i = 0; i < BOARD_WIDTH ; i++)
 			{
 				if(i>=thin && i<BOARD_WIDTH-thin)
-					board[i][j] = 'o';
+					board[j][i] = 'o';
 				else
-					board[i][j] = ' ';					
+					board[j][i] = ' ';					
 			}
-			
+
 			thin++;
 		}
+		
+		thePlayers = new Player[2];
+
+		lastMovePosition = new Position();
 	}
 
 	@Override
 	public boolean checkIfPiecedCanBeDroppedIn(int column) 
 	{
-		return board[0][column] == 'o';
+		return (column >= 0 && column < BOARD_WIDTH) ? board[0][column] == 'o' : false;
 	}
 
 	@Override
@@ -49,119 +49,122 @@ public class Connect4Field implements Connect4FieldInterface {
 	{
 		int i = 0;
 		char buf;
-		if(column >= 0 && column < BOARD_WIDTH)
-		{
-			buf = board[i][column];
-			while(buf == 'o')
+		if(checkIfPiecedCanBeDroppedIn(column))
+		{					
+			do
 			{
 				buf = board[i][column];
+				if (buf != 'o') {
+					break;
+				}
 				i++;
-			}
-			board[i][column] = gamePiece;
-			lastMovePosition.X = i;
-			lastMovePosition.Y = column;
+			} while (i < BOARD_HEIGHT);
+
+			board[i-1][column] = gamePiece;
+			lastMovePosition.X = column;
+			lastMovePosition.Y = i-1;
 		}
 		else
 		{
-			System.out.println("dropPieces wrong input : column : (" + column + ") is out of range");
+			System.out.println("dropPieces wrong input : column : (" + column + ") is out of range or full");
 		}
 	}
 
 	@Override
-	public boolean didLastMoveWin() 
-	{
-			boolean lastMoveWin = false;
-		
-		char piece = board[lastMovePosition.X][lastMovePosition.Y];
-		
+	public boolean didLastMoveWin() {
+		boolean lastMoveWin = false;
+
+		char piece = board[lastMovePosition.Y][lastMovePosition.X];
+
 		int y0 = Math.max(0, lastMovePosition.Y - 1),
 			y1 = Math.min(lastMovePosition.Y + 1, BOARD_HEIGHT - 1),
 			x0 = Math.max(0, lastMovePosition.X - 1),
 			x1 = Math.min(lastMovePosition.X + 1, BOARD_WIDTH - 1);
-		
+
 		here:
-		
-		for (int j = y0; j <= y1; j++) {
-			for (int i = x0; i <= x1; i++) {
-				if (i == lastMovePosition.X && j == lastMovePosition.Y || board[i][j] != piece) {
-					break; // we already know what is going on for the lastMove and only care about same values
-				} else {
-					int direction = (i - lastMovePosition.X != 0) ? j - lastMovePosition.Y / i - lastMovePosition.X : Integer.MAX_VALUE;
-					
-					// use to count if there are at least 4 pieces in a row
-					int countPieces = 0;
-					
-					// we want to try every value 3 position before the lastMovePosition and 3 position after
-					// so as not to miss any case (configuration:  oooxooo)
-					int minX = Math.max(0, lastMovePosition.X - 3),
-						maxX = Math.min(lastMovePosition.X + 3, BOARD_WIDTH -1),
-						minY = Math.max(0, lastMovePosition.Y - 3),
-						maxY = Math.min(lastMovePosition.X - 3, BOARD_HEIGHT -1);
-					// besides we have to handle the diagonals
-					int minXY = Math.max(minX - lastMovePosition.X, minY - lastMovePosition.Y),
-						maxXY = Math.min(maxX - lastMovePosition.X, maxY - lastMovePosition.Y);
-					
-					switch (direction) { // we check if there is 4 pieces in the same direction
-					case -1:
-						for (int elt = minXY; elt < maxXY; elt++) {
-							 if (board[lastMovePosition.X + elt][lastMovePosition.Y - elt] == piece) {
-								 countPieces++;
-								 // if we already have our 4 pieces, we can break
-								 if (countPieces == 4) {
-									 lastMoveWin = true;
-									 break here;
-								 }
-							 } else {
-								 countPieces = 0;
-							 }
+
+			for (int j = y0; j <= y1; j++) {
+				for (int i = x0; i <= x1; i++) {
+					if (i == lastMovePosition.X && j == lastMovePosition.Y || board[j][i] != piece) {
+						continue; // we already know what is going on for the lastMove and only care about same values
+					} else {
+						int direction = (i - lastMovePosition.X != 0) ? j - lastMovePosition.Y / i - lastMovePosition.X : Integer.MAX_VALUE;
+
+						// use to count if there are at least 4 pieces in one row
+						int countPieces = 0;
+
+						// we want to try every value 3 position before the lastMovePosition and 3 position after
+						// so as not to miss any case (configuration:  oooxooo)
+						int minX = Math.max(0, lastMovePosition.X - 3),
+							maxX = Math.min(lastMovePosition.X + 3, BOARD_WIDTH),
+							minY = Math.max(0, lastMovePosition.Y - 3),
+							maxY = Math.min(lastMovePosition.X + 3, BOARD_HEIGHT);
+						// besides we have to handle the diagonals
+						int minXY = Math.max(minX - lastMovePosition.X, minY - lastMovePosition.Y),
+							maxXY = Math.min(maxX - lastMovePosition.X, maxY - lastMovePosition.Y);
+
+						switch (direction) { // we check if there is 4 pieces in the same direction
+						case -1:
+							for (int elt = minXY; elt < maxXY; elt++) {
+								if (board[lastMovePosition.Y - elt][lastMovePosition.X + elt] == piece) {
+									countPieces++;
+									// if we already have our 4 pieces, we can break
+									if (countPieces == 4) {
+										lastMoveWin = true;
+										break here;
+									}
+								} else {
+									countPieces = 0;
+								}
+							}
+							break;
+						case 0:
+							for (int elt = minX; elt < maxX; elt++) {
+								if (board[lastMovePosition.Y][elt] == piece) {
+									countPieces++;
+									// if we already have our 4 pieces, we can break
+									if (countPieces == 4) {
+										lastMoveWin = true;
+										break here;
+									}
+								} else {
+									countPieces = 0;
+								}
+							}
+							break;
+						case 1:
+							for (int elt = minXY; elt < maxXY; elt++) {
+								if (board[lastMovePosition.Y + elt][lastMovePosition.X + elt] == piece) {
+									countPieces++;
+									// if we already have our 4 pieces, we can break
+									if (countPieces == 4) {
+										lastMoveWin = true;
+										break here;
+									}
+								} else {
+									countPieces = 0;
+								}
+							}
+							break;
+						default: // when we have MAX_VALUE
+							for (int elt = minY; elt < maxY; elt++) {
+								if (board[elt][lastMovePosition.X] == piece) {
+									countPieces++;
+									// if we already have our 4 pieces, we can break
+									if (countPieces == 4) {
+										lastMoveWin = true;
+										break here;
+									}
+								} else {
+									countPieces = 0;
+								}
+							}
+							break;
 						}
-						break;
-					case 0:
-						for (int elt = minX; elt < maxX; elt++) {
-							 if (board[elt][lastMovePosition.Y] == piece) {
-								 countPieces++;
-								 // if we already have our 4 pieces, we can break
-								 if (countPieces == 4) {
-									 lastMoveWin = true;
-									 break here;
-								 }
-							 } else {
-								 countPieces = 0;
-							 }
-						}
-						break;
-					case 1:
-						for (int elt = minXY; elt < maxXY; elt++) {
-							 if (board[lastMovePosition.X + elt][lastMovePosition.Y + elt] == piece) {
-								 countPieces++;
-								 // if we already have our 4 pieces, we can break
-								 if (countPieces == 4) {
-									 lastMoveWin = true;
-									 break here;
-								 }
-							 } else {
-								 countPieces = 0;
-							 }
-						}
-						break;
-					default: // when we have MAX_VALUE
-						for (int elt = minY; elt < maxY; elt++) {
-							 if (board[lastMovePosition.X][elt] == piece) {
-								 countPieces++;
-								 // if we already have our 4 pieces, we can break
-								 if (countPieces == 4) {
-									 break;
-								 }
-							 } else {
-								 countPieces = 0;
-							 }
-						}
-						break;
 					}
 				}
 			}
-		}
-		
+
 		return lastMoveWin;
 	}
 
@@ -182,10 +185,8 @@ public class Connect4Field implements Connect4FieldInterface {
 	public void init(PlayerInterface playerA, PlayerInterface playerB)
 	{		
 		// modify attributes in the instance of Connect4Field
-		player1 = playerA;
-		player2 = playerB;
-		isPlayer1Turn = true;
-		lastMovePosition = new Position();
+		thePlayers[0] = playerA;
+		thePlayers[1] = playerB;
 	}
 
 	@Override
@@ -195,9 +196,9 @@ public class Connect4Field implements Connect4FieldInterface {
 		{
 			for (int i = 0; i < BOARD_WIDTH ; i++)
 			{
-				displayBoard += board[i][j];					
+				displayBoard += board[j][i];					
 			}
-			
+
 			displayBoard += '\n';
 		}
 		return displayBoard;
@@ -206,67 +207,30 @@ public class Connect4Field implements Connect4FieldInterface {
 	@Override
 	public void playTheGame()
 	{
-		int indexPlay;
-		while(!endgame)
-		{
-			toString();
-			if(isItaDraw())
-			{
-				endgame = true;
-			}
-			else
-			{
-				if(isPlayer1Turn)
-				{
-					System.out.println("Player 1 turn,");
-					indexPlay = player1.nextMove();
-					if(checkIfPiecedCanBeDroppedIn(indexPlay))
-					{
-						dropPieces(indexPlay,player1.getGamePiece());
-						if(didLastMoveWin())
-						{
-							endgame = true;
-							System.out.println("Player 1 win !");
-							break;
-						}
-						else
-						{
-							isPlayer1Turn = false;
-						}
-					}
-					else
-					{
-						System.out.println("You can't place a piece in this column ! play again");
-						continue;
-					}
-				}
-				else
-				{
-					System.out.println("Player 1 turn,");
-					indexPlay = player2.nextMove();
-					if(checkIfPiecedCanBeDroppedIn(indexPlay))
-					{
-						dropPieces(indexPlay,player2.getGamePiece());
-						if(didLastMoveWin())
-						{
-							endgame = true;
-							System.out.println("Player 2 win !");
-							break;
-						}
-						else
-						{
-							isPlayer1Turn = true;
-						}
-					}
-					else
-					{
-						System.out.println("You can't place a piece in this column ! play again");
-						continue;
+		int column;
+//		System.out.println(this);
+		boolean gameIsOver = false;
+
+		do {
+			for ( int index = 0; index < 2; index ++ )      {
+				System.out.println(this);
+				if ( isItaDraw() )      {
+					System.out.println("Draw");
+					gameIsOver = true;
+
+				} else {
+					column = thePlayers[index].nextMove();
+					dropPieces(column, thePlayers[index].getGamePiece() );
+					
+					if ( didLastMoveWin() ) {
+						gameIsOver = true;
+						System.out.println("The winner is: " + thePlayers[index].getName());
 					}
 				}
 			}
-			
-		}
+
+		}  while ( ! gameIsOver  );
+
 	}
 
 }
