4a6d2f32cf70ffdceb6162b0cde7a7aef9ef3857
Gurvan <g.lecuyer22@gmail.com>
2015-09-26 11:27:22 -0400

    Ajout StorageDynamic et StorageTest

--- CSCI_605_Homework_05/src/exercise/n1/StorageDynamic.java
+++ CSCI_605_Homework_05/src/exercise/n1/StorageDynamic.java
@@ -15,36 +15,69 @@ package exercise.n1;
 
 public class StorageDynamic<E, V> implements Storage<E, V> {
 
-	Object[] storage = null;
-	int capacity;
+	Object[] storage;
 	int currentIndex;
-	static final int MAX_CAPACITY = 100, CAPACITY = 10;
 	
 	/**
 	 * 
 	 */
 	public StorageDynamic() {
-		currentIndex = 0;
-		capacity = Math.min(CAPACITY, MAX_CAPACITY);
-		storage = new Object[this.capacity];
+		currentIndex = -1;
+		storage = null;
 	}
 
 	public boolean add(E e) {
-		if (currentIndex < capacity) {
-			storage[capacity++] = e;
-			return true;
+		if(currentIndex == -1)
+		{
+			storage = new Object[1];
+			currentIndex = 0;
 		}
-		return false;
+		else
+		{
+			currentIndex++;
+			Object[] buf = new Object[currentIndex + 1];
+			for(int i = 0; i < buf.length - 1; i++)
+			{
+				buf[i] = storage[i];
+			}
+			storage = buf;
+		}
+		storage[currentIndex] = e;
+		return true;
 	}
 
-	public boolean add(int index, E element) {
-		if (currentIndex < capacity) {
-			E e = (E) storage[index];
-			storage[index] = element;
-			storage[capacity++] = e;
-			return true;
+	public boolean add(int index, E element) 
+	{
+		if(currentIndex == -1 && index != 0)
+		{
+			return false;
 		}
-		return false;
+		else
+		{
+			int j;
+			Object[] buf = new Object[currentIndex + 2];
+			for(int i = 0; i < buf.length - 1; i++)
+			{
+				if(i <= index)
+				{
+					j = i;
+				}
+				else
+				{
+					j = i+1;
+				}
+				if(i == index)
+				{
+					buf[i] = element;
+				}
+				else
+				{	
+					buf[i] = storage[j];
+				}
+			}
+			storage = buf;
+		}
+		return true;
 	}
 
 	public void addElement(E obj) {
@@ -54,32 +87,67 @@ public class StorageDynamic<E, V> implements Storage<E, V> {
 
 	public void addElement(E obj, V elem) {
 		add(obj);
-		if (currentIndex < capacity) {
-			storage[capacity++] = elem;
+		if(currentIndex == -1)
+		{
+			storage = new Object[1];
+			currentIndex = 0;
 		}
-		
-		
+		else
+		{
+			currentIndex++;
+			Object[] buf = new Object[currentIndex + 1];
+			for(int i = 0; i < buf.length - 1; i++)
+			{
+				buf[i] = storage[i];
+			}
+			storage = buf;
+		}
+		storage[currentIndex] = elem;
 	}
 
-	public int capacity() {
-		return capacity;
+	public int capacity() 
+	{
+		if(currentIndex != -1)
+		{
+		return storage.length;
+		}
+		else 
+			return -1;
 	}
 
-	public void clear() {
-		storage = new Object[capacity];
-		
+	public void clear() 
+	{
+		storage = null;
+		currentIndex = -1;
 	}
 
-	public E firstElement() {
+	public E firstElement() 
+	{
+		if(currentIndex != -1)
+		{
 		return (E)storage[0];
+		}
+		else
+			return null;
 	}
 
-	public E get(int index) {
-		return (E)storage[index];
+	public E get(int index) 
+	{
+		if(currentIndex != -1)
+		{
+		return (E)storage[index-1];
+		}
+		else
+			return null;
 	}
 
 	public E lastElement() {
-		return (E)storage[currentIndex - 1];
+		if(currentIndex != -1)
+		{
+		return (E)storage[currentIndex];
+		}
+		else
+			return null;
 	}
 
 
--- CSCI_605_Homework_05/src/exercise/n1/StorageTest.java
+++ CSCI_605_Homework_05/src/exercise/n1/StorageTest.java
@@ -12,8 +12,33 @@ package exercise.n1;
 
 
 public class StorageTest {
-    public static void main(String args[])	{
+    public static void main(String args[])	
+    {
     	Storage<String, String> aStorageString = new StorageFixed<String, String>();
     	Storage<Integer, String> aStorageInteger = new StorageFixed<Integer, String>();
+    	
+    	Storage<String, String> bStorageString = new StorageDynamic<String, String>();
+    	Storage<Integer, String> bStorageInteger = new StorageDynamic<Integer, String>();
+    	
+    	//aStorageString.add("Test String fixed");
+    	//System.out.println(" a String : " + aStorageString.firstElement());
+    	//aStorageInteger.add(1);
+    	//System.out.println(" a Integer : " + aStorageInteger.firstElement());
+    	
+    	bStorageString.add("Test String dynamic 1 ");
+    	System.out.println("Test firstElement String : " + bStorageString.firstElement());
+    	bStorageString.add("Test add dynamic 2 ");
+    	bStorageString.addElement("Test addElement(E) dynamic 3 ");
+    	bStorageString.addElement("Test addElement(E, V) dynamic 4 ", " Test addElement(E, V) and 5");
+    	System.out.println("Test get(index) dynamic 2 : " +bStorageString.get(2));
+    	System.out.println("Test get(index) dynamic 3 : " +bStorageString.get(3));
+    	System.out.println("Test get(index) dynamic 4 : " +bStorageString.get(4));
+    	System.out.println("Test lastElement String 5 : " + bStorageString.lastElement());
+    	System.out.println("Test capacity String : " +bStorageString.capacity());
+    	
+    	bStorageString.clear();
+
+    	System.out.println("Test firstElement String : " + bStorageString.firstElement());
+    	
     }
 }
