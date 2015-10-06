d4b0bae815252578ed4795a956c6f5e6d594f25e
guroy <guroy@insa-rennes.fr>
2015-10-05 21:04:31 -0400

    skeleton HW7

---
+++ CSCI_605_Homework_07/src/exercise/n1/StringZipOutputStream.java
@@ -0,0 +1,30 @@
+package exercise.n1;
+
+/**
+* Create a StringZipOutputStream class, similar to GZipOutputStream
+*
+*
+* @version   $Id: StringZipOutputStream.java,v 1.0 2015/10/05 $
+* @author    Gurvan Lecuyer
+* @author    Guillaume Roy
+*
+* Revisions: 
+*
+* 	Initial revision
+*
+*/
+
+import java.io.OutputStream;
+
+public class StringZipOutputStream	{
+
+	// Creates a new output stream with a default buffer size.
+	public StringZipOutputStream(OutputStream out)	{
+	}
+	// Writes aStrign compressed output stream. This method will block until all data is written.
+	public void write(String aString) {
+	}
+	// Writes remaining data to the output stream and closes the underlying stream.
+	public void close() {
+	}
+} 
---
+++ CSCI_605_Homework_07/src/exercise/n2/StringZipInputStream.java
@@ -0,0 +1,30 @@
+package exercise.n2;
+
+/**
+* Create a StringZipInputStream class, similar to GZipInputStream
+*
+*
+* @version   $Id: StringZipInputStream.java,v 1.0 2015/10/05 $
+* @author    Gurvan Lecuyer
+* @author    Guillaume Roy
+*
+* Revisions: 
+*
+* 	Initial revision
+*
+*/
+
+import java.io.InputStream;
+
+public class StringZipInputStream	{
+
+	// Creates a new input stream with a default buffer size.
+	public StringZipInputStream(InputStream out)	{
+	}
+	// Reads data into a string. the method will block until some input can be read; otherwise, no bytes are read and null is returned.
+//	public String read() {
+//	}
+	// Closes this input stream and releases any system resources associated with the stream.
+	public void close() {
+	}
+} 
