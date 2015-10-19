364e80c0e94920340ecfaebf97e0c8170e664da4
Gurvan <g.lecuyer22@gmail.com>
2015-10-19 13:16:47 -0400

    Fin de l'exercice 3

--- CSCI_605_Homework_08/src/exercise/n3/T_2.java
+++ CSCI_605_Homework_08/src/exercise/n3/T_2.java
@@ -25,28 +25,28 @@ public class T_2 extends Thread    {
     }      
         
     public static void main (String args []) {
-        new T_2(1).start();;
-        new T_2(2).start();;
+        new T_2(1).start();
+        new T_2(2).start();
             
         System.out.println("theValue = " + theValue );
         System.out.println("theValue = " + theValue );
         
         // Possible cases : (with examples)
  		/*
+ 		 * theValue = 1		If T_2(1).run() is executed after the 
+ 		 * theValue = 2		println method and then T_2.run() is executed
+ 		 * 
+ 		 * theValue = 1		If both println are executed before both run method
  		 * theValue = 1
+ 		 * 
+ 		 * theValue = 3		If both run method are executed before 
+ 		 * theValue = 2		the println method and T_2(1).run is executed first
+ 		 * 
+ 		 * theValue = 2		only T_2(2).run is executed before the println
  		 * theValue = 2
  		 * 
- 		 * theValue = 1
- 		 * theValue = 1
- 		 * 
- 		 * theValue = 3
- 		 * theValue = 2
- 		 * 
- 		 * theValue = 2
- 		 * theValue = 2
- 		 * 
- 		 * theValue = 1
- 		 * theValue = 3
+ 		 * theValue = 1		the println method of T_2(1) is executed 
+ 		 * theValue = 3		and then T_2(1).run() then T_2(1).println is executed
  		 */
     }       
 }  
