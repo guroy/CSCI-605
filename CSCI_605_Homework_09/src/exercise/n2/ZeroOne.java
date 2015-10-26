eb97904437ec45d7a58dfd2c1dce3997c06e7302
guroy <guroy@insa-rennes.fr>
2015-10-26 14:44:29 -0400

    bof

--- CSCI_605_Homework_09/src/exercise/n2/ZeroOne.java
+++ CSCI_605_Homework_09/src/exercise/n2/ZeroOne.java
@@ -15,20 +15,26 @@ public class ZeroOne extends Thread	{
 	private String info;
 	static Object o = new Object();
 	static boolean oneIsRunning = false;
+	static int index = 0;
 					     
 	public ZeroOne (String info) {
 		this.info    = info;
 	}
+	
 	public void run () {
 		while ( true )	{
 			synchronized ( o ) {
 				o.notify();
 				System.out.print(info);
 				try {
-					if ( ! oneIsRunning )	{
-						( new ZeroOne("1") ).start();
-						oneIsRunning = true;
-					}
+					if (index == 0) {
+						index = Integer.parseInt(info);
+						System.out.print(info);
+						for (int i = 0; i < 99; i++)
+							new ZeroOne(Integer.toString(index++)).start();
+
+					} 
+
 					sleep(300);
 					o.wait();
 				} catch ( Exception e ) { }
