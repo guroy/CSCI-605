c3f614966b8073a58cb1795899aad281bfa3cc8b
guroy <guroy@insa-rennes.fr>
2015-12-07 13:19:33 -0500

    bannae

--- CSCI_605_Homework_14/src/n1/HashSetNew.java
+++ CSCI_605_Homework_14/src/n1/HashSetNew.java
@@ -3,13 +3,34 @@ package n1;
 import java.util.*;
 
 public class HashSetNew<E> extends HashSet<E> {
-	/**
-	 * 
-	 */
+	// prevent some 
 	private static final long serialVersionUID = 1L;
+	
+	// the capacity of the table is always power of 2
+	final static int MIN_CAPACITY = 16; // 2^4
+	final static int MAX_CAPACITY = 1073741824; // 2^30
+	// when the table is more than 75% filled we rehash the table
+	final static float LOAD_FACTOR = 0.75f;
+	// the table that contains the elements
+	Object[] table;
+	// the size of the table
+	int size;
+	// The next size value at which to resize 
+	int threshold;
+	
+	public HashSetNew() {
+		table = new Object[MIN_CAPACITY];
+		threshold = (int) (MIN_CAPACITY * LOAD_FACTOR);
+		size = 0;
+	}
 
 	public boolean add(E e) {
-		return true;
+		// we need to be sure that the element does not already exist
+		if (!contains(e)) {
+			size++;
+			return true;
+		}
+		return false;
 	}
 	
 	public void clear() {
@@ -21,7 +42,7 @@ public class HashSetNew<E> extends HashSet<E> {
 	}
 	
 	public boolean isEmpty() {
-		return true;
+		return size == 0;
 	}
 	
 	public Iterator<E> iterator() {
@@ -29,10 +50,14 @@ public class HashSetNew<E> extends HashSet<E> {
 	}
 	
 	public boolean remove(Object o) {
-		return true;
+		if (contains(o)) {
+			size--;
+			return true;
+		}
+		return false;
 	}
 	
 	public int size() {
-		return 0;
+		return size;
 	}
 }
